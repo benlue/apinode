9727d4a922cbd65f26c6fc3244dcff071188b506
lychee <lychee@gocharm.com.tw>
2016-08-26 13:52:54 +0800

    set default language by browser setting

--- www/apinode/themes/apinode/blocks/views/api/header/header.js
+++ www/apinode/themes/apinode/blocks/views/api/header/header.js
@@ -9,7 +9,15 @@ ctrl.logout = function() {
 }
 
 ctrl.startup = function(){	
-	
+	var loc = getCookie("_xs_locale");
+	if(!loc){
+		var lang = window.navigator.languages ? window.navigator.languages[0] : window.navigator.userLanguage || window.navigator.language;
+		if(lang.indexOf("zh") >= 0)
+			ctrl.setLanguage("zh");
+		else
+			ctrl.setLanguage("en");
+		window.reload();
+	}
 }
 
 ctrl.setLanguage = function(language) {
@@ -31,4 +39,15 @@ function setCookie(cname, cvalue, exdays) {
     d.setTime(d.getTime() + (exdays*24*60*60*1000));
     var expires = "expires="+d.toUTCString();
     document.cookie = cname + "=" + cvalue + "; " + expires + "; path=/";
+}
+
+function getCookie(cname) {
+	var name = cname + "=";
+	var ca = document.cookie.split(';');
+	for(var i=0; i<ca.length; i++) {
+		var c = ca[i];
+		while (c.charAt(0)==' ') c = c.substring(1);
+		if (c.indexOf(name) == 0) return c.substring(name.length,c.length);
+	}
+	return "";
 }
